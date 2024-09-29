ae9652fc7b4c9250002cffdffa900fd44e31982c
ds-1234 <dhruv.26saraswat@gmail.com>
2024-09-29 11:52:38 +0530

    changes in class entity and subject

--- src/main/java/com/project/school/management/entity/ClassEntity.java
+++ src/main/java/com/project/school/management/entity/ClassEntity.java
@@ -3,14 +3,12 @@ package com.project.school.management.entity;
 import java.util.ArrayList;
 import java.util.List;
 
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.OneToMany;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import lombok.Data;
 
@@ -31,9 +29,8 @@ public class ClassEntity {
 
 	@Column(name = "section", nullable = false)
 	private String section;
-
-	@OneToMany(cascade = CascadeType.MERGE)
-	@JoinColumn(name = "subject_id", referencedColumnName = "id")
+	
+	@ManyToMany
 	List<Subject> subject = new ArrayList<>();
 
 }
--- src/main/java/com/project/school/management/entity/Subject.java
+++ src/main/java/com/project/school/management/entity/Subject.java
@@ -1,10 +1,14 @@
 package com.project.school.management.entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import lombok.Data;
 
@@ -21,5 +25,8 @@ public class Subject {
 
 	@Column(name = "description", nullable = false)
 	private String description;
+	
+	 @ManyToMany
+	List<ClassEntity> classEntity = new ArrayList<>();
 
 }
