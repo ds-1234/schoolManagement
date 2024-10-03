448d4d62e4afd0c66d9ca0c009a55344f9469f90
ds-1234 <dhruv.26saraswat@gmail.com>
2024-10-03 22:07:09 +0530

    changes in notice entity

--- src/main/java/com/project/school/management/entity/NoticeEntity.java
+++ src/main/java/com/project/school/management/entity/NoticeEntity.java
@@ -2,11 +2,15 @@ package com.project.school.management.entity;
 
 import java.sql.Date;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.Data;
 
@@ -32,5 +36,9 @@ public class NoticeEntity {
 	
 	@Column(name = "notice_id", nullable = false)
 	private String noticeId;
+	
+	@JsonIgnoreProperties({ "hibernateLazyInitializer", "handler" })
+	@ManyToOne(fetch = FetchType.LAZY)
+	private Role role;
 
 }
